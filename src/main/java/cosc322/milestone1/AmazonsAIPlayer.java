14ce9a9c2b1e4b36f7b3950ecb2d6dcef01e0efd
ThomPetrus <46361901+ThomPetrus@users.noreply.github.com>
2021-02-18 12:14:50 -0800

    Update AmazonsAIPlayer.java

--- src/main/java/cosc322/milestone1/AmazonsAIPlayer.java
+++ src/main/java/cosc322/milestone1/AmazonsAIPlayer.java
@@ -99,56 +99,60 @@ public class AmazonsAIPlayer extends GamePlayer {
 		ArrayList<Integer> queen = new ArrayList<>();
 		ArrayList<Integer> newPos = new ArrayList<>();
 		ArrayList<Integer> arrowPos = new ArrayList<>();
+		boolean valid = false;
 
 		// Should have a check if pieces are already blocked in?
 		ArrayList<ArrayList<Integer>> possibleMoves = gameBoard.getPossibleMoves(isWhitePlayer);
 		if (!possibleMoves.isEmpty()) {
 
-			ArrayList<Integer> move = randomMove(possibleMoves);
-			newPos.add(move.get(0)); // new pos
-			newPos.add(move.get(1));
-			queen.add(move.get(2)); // queen being moved y, x
-			queen.add(move.get(3));
+			do {
+				ArrayList<Integer> move = randomMove(possibleMoves);
+				newPos.add(move.get(0)); // new pos
+				newPos.add(move.get(1));
+				queen.add(move.get(2)); // queen being moved y, x
+				queen.add(move.get(3));
 
-			ArrayList<ArrayList<Integer>> possibleArrows = gameBoard.getPossibleMoves(newPos);
-//			if (!possibleArrows.isEmpty()) {
-				ArrayList<Integer> arrowMove = randomMove(possibleArrows);
-				arrowPos.add(arrowMove.get(0)); // arrow position
-				arrowPos.add(arrowMove.get(1));
+				ArrayList<ArrayList<Integer>> possibleArrows = gameBoard.getPossibleMoves(newPos);
+				if (!possibleArrows.isEmpty()) {
+					ArrayList<Integer> arrowMove = randomMove(possibleArrows);
+					arrowPos.add(arrowMove.get(0)); // arrow position
+					arrowPos.add(arrowMove.get(1));
 
-				gameBoard.updateBoard(queen, newPos, arrowPos);
-				gamegui.updateGameState(queen, newPos, arrowPos);
-				gameClient.sendMoveMessage(queen, newPos, arrowPos);
+					gameBoard.updateBoard(queen, newPos, arrowPos);
+					gamegui.updateGameState(queen, newPos, arrowPos);
+					gameClient.sendMoveMessage(queen, newPos, arrowPos);
 
-				System.out.println("Current Board Matrix:\n------------------------------");
-				byte[][] matrix = gameBoard.getMatrix();
-				for (int row = 0; row < 11; row++) {
-					for (int col = 0; col < 11; col++) {
-						System.out.printf("%d, ", matrix[row][col]);
+					System.out.println("Current Board Matrix:\n------------------------------");
+					byte[][] matrix = gameBoard.getMatrix();
+					for (int row = 0; row < 11; row++) {
+						for (int col = 0; col < 11; col++) {
+							System.out.printf("%d, ", matrix[row][col]);
+						}
+						System.out.println("");
 					}
+
 					System.out.println("");
-				}
+					System.out.println("black: " + gameBoard.getBlackQueens());
+					System.out.println("white: " + gameBoard.getWhiteQueens());
+					System.out.println("arrows: " + gameBoard.getArrows());
 
-				System.out.println("");
-				System.out.println("black: " + gameBoard.getBlackQueens());
-				System.out.println("white: " + gameBoard.getWhiteQueens());
-				System.out.println("arrows: " + gameBoard.getArrows());
-
-				try {
-					TimeUnit.SECONDS.sleep(1);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					try {
+						TimeUnit.SECONDS.sleep(1);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+					valid=true;
+					
+				} else {
+					System.out.println("No more places to put arrows.");
 				}
-				
-//			} else {
-//				System.out.println("No more places to put arrows.");
-//			}
+			} while (!valid);
 		} else {
 			String player = isWhitePlayer ? "White Player" : "Black Player";
 			System.out.println("Game over... " + player);
 		}
-
 	}
 
 	// Will need a class / function

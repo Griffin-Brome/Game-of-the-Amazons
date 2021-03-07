dceb6f7b4b0004ceb1ba3906501d96ac840d4493
opeyem1a <opeyadeyemi@gmail.com>
2021-03-06 21:28:18 -0800

    Reduced code complexity of ActionFactoryRecursive

--- src/main/java/cosc322/milestone1/ActionFactoryRecursive.java
+++ src/main/java/cosc322/milestone1/ActionFactoryRecursive.java
@@ -1,28 +1,15 @@
 package cosc322.milestone1;
 
-//import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+import static utils.Constant.*;
+import static utils.GameLogic.*;
 
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ActionFactoryRecursive {
-    private static final byte U = 1;
-    private static final byte UR = 2;
-    private static final byte R = 3;
-    private static final byte DR = 4;
-    private static final byte D = 5;
-    private static final byte DL = 6;
-    private static final byte L = 7;
-    private static final byte UL = 8;
-
-    private static final byte[] directions = {U, UR, R, DR, D, DL, L, UL};
-
-    public static final int N = 10; // N in an N x N board
-
-    private ArrayList<byte[]> whiteQueens;
-    private ArrayList<byte[]> blackQueens;
-    private ArrayList<byte[]> arrows;
+    private ArrayList<Queen> whiteQueens;
+    private ArrayList<Queen> blackQueens;
+    private ArrayList<Arrow> arrows;
     byte[][] boardMatrix;
     ArrayList<byte[]> moves;
 
@@ -33,20 +20,18 @@ public class ActionFactoryRecursive {
         arrows = gameBoard.getArrows();
         moves = new ArrayList<>();
 
+        //TODO: remove this test print string
         System.out.println(Arrays.deepToString(boardMatrix));
     }
 
     public ArrayList<byte[]> getPossibleMoves(boolean isWhitePlayer) {
         ArrayList<byte[]> moves = new ArrayList<>();
-        if (isWhitePlayer) {
-            for (byte[] queenPos : whiteQueens) {
-                moves.addAll(getPossibleMoves(queenPos));
-            }
-        } else {
-            for (byte[] queenPos : blackQueens) {
-                moves.addAll(getPossibleMoves(queenPos));
-            }
+        ArrayList<Queen> ourQueens = isWhitePlayer ? whiteQueens : blackQueens;
+
+        for (Queen queen : ourQueens) {
+            moves.addAll(getPossibleMoves(queen.getPosition()));
         }
+
         return moves;
     }
 
@@ -58,23 +43,21 @@ public class ActionFactoryRecursive {
      * @return the set of all possible moves from pos
      */
     public ArrayList<byte[]> getPossibleMoves(byte[] oldPos) {
-        ArrayList<byte[]> possibleMoves = generateMovesStart(oldPos);
-//        System.out.println(Arrays.deepToString(possibleMoves.toArray()));
-
+        ArrayList<byte[]> possibleMoves = generateMovesHelper(oldPos);
 
         for (byte[] possibleMove : possibleMoves) {
-            addMove(oldPos, possibleMove[1], possibleMove[0]);
+            addMove(oldPos, possibleMove[0], possibleMove[1]);
         }
 
         return moves;
     }
 
-    private void addMove(byte[] oldPos, byte newPosY, byte newPosX) {
+    private void addMove(byte[] oldPos, byte newPosX, byte newPosY) {
         byte[] move = new byte[4];
         // the "+1" is because the server indexes from 1-10 and we index from 0-9
         // the 2 lines after this represent the new position coordinates of the queen
-        move[0] = (byte) (newPosY + 1);
-        move[1] = (byte) (newPosX + 1);
+        move[0] = newPosX;
+        move[1] = newPosY;
         // queen original position
         move[2] = oldPos[0];
         move[3] = oldPos[1];
@@ -82,64 +65,22 @@ public class ActionFactoryRecursive {
         moves.add(move);
     }
 
-    public ArrayList<byte[]> generateMovesStart(byte[] currPos) {
+    public ArrayList<byte[]> generateMovesHelper(byte[] currPos) {
         ArrayList<byte[]> possibleMoves = new ArrayList<>();
-        for (byte dir : directions) {
-            byte[] newPos = newPosition(dir, currPos.clone());
+        for (byte dir : DIRECTIONS) {
+            byte[] newPos = generateNewPosition(currPos.clone(), dir);
             _generateMoves(dir, newPos, possibleMoves);
         }
         return possibleMoves;
     }
 
-    public void _generateMoves(byte direction, byte[] currPos, ArrayList<byte[]> possibleMoves) {
-        if (!isValidPosition(currPos)) return;
+    public void _generateMoves(byte dir, byte[] currPos, ArrayList<byte[]> possibleMoves) {
+        // add the current move to the set of all possible moves
         possibleMoves.add(currPos);
-        byte[] newPos = newPosition(direction, currPos.clone());
-        _generateMoves(direction, newPos, possibleMoves);
+        byte[] newPos = generateNewPosition(currPos.clone(), dir);
 
-    }
-
-    public static byte[] newPosition(byte dir, byte[] oldPos) {
-        switch (dir) {
-            case U:
-                return new byte[]{oldPos[0], --oldPos[1]};
-            case UR:
-                return new byte[]{++oldPos[0], --oldPos[1]};
-            case R:
-                return new byte[]{++oldPos[0], oldPos[1]};
-            case DR:
-                return new byte[]{++oldPos[0], ++oldPos[1]};
-            case D:
-                return new byte[]{oldPos[0], ++oldPos[1]};
-            case DL:
-                return new byte[]{--oldPos[0], ++oldPos[1]};
-            case L:
-                return new byte[]{--oldPos[0], oldPos[1]};
-            case UL:
-                return new byte[]{--oldPos[0], --oldPos[1]};
-            default:
-                return new byte[]{-1, -1};
-        }
-    }
-
-    private boolean isValidPosition(byte[] position) {
-        return position[0] >= 0 && position[0] < N && position[1] >= 0 && position[1] < N && !isOccupied(position);
-    }
-
-    private boolean isOccupied(byte[] position) {
-        return boardMatrix[position[0]][position[1]] != 0;
-    }
-
-
-    private boolean isBlank(byte boardMatrix1) {
-        return boardMatrix1 == Constant.BLANK;
-    }
-
-    public ArrayList<byte[]> getWhiteQueens() {
-        return whiteQueens;
-    }
-
-    public ArrayList<byte[]> getBlackQueens() {
-        return blackQueens;
+        // if the new position is valid, explore it
+        if (isValidPosition(boardMatrix, currPos))
+            _generateMoves(dir, newPos, possibleMoves);
     }
 }
